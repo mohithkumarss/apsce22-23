a5827014bf4c9f1ad42fb303916d2c789fa1742b
LeokondaMohith <109334910+mohithkumarss@users.noreply.github.com>
2023-03-26 01:45:24 +0530

    26mar23
    department pages.

---
+++ public_html/departments/dep.js
@@ -0,0 +1,32 @@
+$(function () {
+  $("#nav-placeholder").load("/public_html/navbar.html");
+});
+
+$(function () {
+  $("#footer-placeholder").load("/public_html/footer.html");
+});
+$(function () {
+  $("#back-to-top").load("/public_html/backtotop/index.html");
+});
+$(function () {
+  $("#back-to-top").load("/public_html/backtotop/index.html");
+});
+
+var position = 500;
+$(".leftBtn").click(function () {
+  $(".overflow").animate(
+    {
+      scrollLeft: "-=" + position,
+    },
+    "slow"
+  );
+});
+
+$(".rightBtn").click(function () {
+  $("div").animate(
+    {
+      scrollLeft: "+=" + position,
+    },
+    "slow"
+  );
+});
